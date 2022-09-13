96f827dc01a6e29d7372ec2400716008909934a4
vindyzulf <davina.fardya@ui.ac.id>
2022-09-13 17:08:31 +0700

    commit tugas 2

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+def show_catalog(request):
+    return render(request, "katalog.html", context)
+
+data_barang_katalog = CatalogItem.objects.all()
+context = {
+    'list_barang': data_barang_katalog,
+    'nama': 'Davina'
+}
